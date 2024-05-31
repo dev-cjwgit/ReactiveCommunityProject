e700e0d6d15ec3a7271d9cf53832fb0e7b3a0c4b
최진우 <cjw.git@gmail.com>
2024-05-31 21:07:18 +0900

    refactor: 비즈니스 로직 주석 작성

--- src/main/kotlin/com/devcjw/reactivecommunity/board/service/impl/BoardServiceImpl.kt
+++ src/main/kotlin/com/devcjw/reactivecommunity/board/service/impl/BoardServiceImpl.kt
@@ -17,52 +17,71 @@ import reactor.core.publisher.Mono
 @Service
 @RequiredArgsConstructor
 class BoardServiceImpl(
-        private val boardDAO: BoardDAO,
+    private val boardDAO: BoardDAO,
 ) : BoardService {
     override fun list(rcUser: RcUserJwtClaims, bbsPath: String): Flux<RestResponseVO<BoardRepListVO>> {
+        /**
+         * 1. 게시판 존재 확인
+         * 2. 게시글 목록 가져오기
+         * 3. 결과 맵핑 및 반환
+         */
         return boardDAO.isBbsPath(bbsPath)
-                .filter { exists -> exists }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
-                .flatMapMany {
-                    boardDAO.selectList(bbsPath)
-                }
-                .map {
-                    val boardRepListVO = BoardRepListVO(it.uid, it.title, it.writerNickname, it.hit, it.createdAt, it.updatedAt)
-                    RestResponseVO(
-                            result = true,
-                            data = boardRepListVO
-                    )
-                }
+            // 1
+            .filter { exists -> exists }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
+            // 2
+            .flatMapMany { boardDAO.selectList(bbsPath) }
+            .map {
+                // 3
+                val boardRepListVO =
+                    BoardRepListVO(it.uid, it.title, it.writerNickname, it.hit, it.createdAt, it.updatedAt)
+                RestResponseVO(
+                    result = true,
+                    data = boardRepListVO
+                )
+            }
     }
 
     override fun detail(
-            rcUser: RcUserJwtClaims,
-            bbsPath: String,
-            boardUid: Long
+        rcUser: RcUserJwtClaims,
+        bbsPath: String,
+        boardUid: Long
     ): Mono<RestResponseVO<BoardRepDetailVO>> {
+        /**
+         * 1. 게시판 존재 확인
+         * 2. 게시글 존재 확인
+         * 3. 데이터 가져오기
+         * 4. 결과 맵핑
+         * 5. 결과 반환
+         */
         return boardDAO.isBbsPath(bbsPath)
-                .filter { exists -> exists }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
-                .flatMap {
-                    boardDAO.selectDetail(boardUid)
-                }
-                .map {
-                    BoardRepDetailVO(
-                            uid = it.uid,
-                            title = it.title,
-                            contents = it.contents,
-                            writerNickname = it.writerNickname,
-                            hit = it.hit,
-                            createdAt = it.createdAt,
-                            updatedAt = it.updatedAt
-                    )
-                }
-                .map {
-                    RestResponseVO(
-                            result = true,
-                            data = it
-                    )
-                }
+            // 1
+            .filter { exists -> exists }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
+            // 2
+            .filterWhen { boardDAO.isBoardUid(boardUid) }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BOARD_EXCEPTION)))
+            // 3
+            .flatMap { boardDAO.selectDetail(boardUid) }
+            // 4
+            .map {
+                BoardRepDetailVO(
+                    uid = it.uid,
+                    title = it.title,
+                    contents = it.contents,
+                    writerNickname = it.writerNickname,
+                    hit = it.hit,
+                    createdAt = it.createdAt,
+                    updatedAt = it.updatedAt
+                )
+            }
+            // 5
+            .map {
+                RestResponseVO(
+                    result = true,
+                    data = it
+                )
+            }
     }
 
     override fun insert(rcUser: RcUserJwtClaims, boardReqInsertDTO: BoardReqInsertDTO): Mono<RestResponseVO<Void>> {
@@ -70,27 +89,27 @@ class BoardServiceImpl(
          * 1. 게시판 존재 확인
          * 2. Entity 생성
          * 3. 데이터 삽입
+         * 4. 성공 반환
          */
         return Mono.just(rcUser)
-                .flatMap {
-                    boardDAO.isBbsUid(boardReqInsertDTO.bbsUid)
-                }
-                .filter { exists -> exists }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
-                .map {
-                    // 2
-                    BoardInsertDTO(
-                            boardReqInsertDTO.bbsUid,
-                            boardReqInsertDTO.title,
-                            boardReqInsertDTO.contents,
-                            rcUser.uid
-                    )
-                }
-                .flatMap {
-                    // 3
-                    boardDAO.insert(it)
-                }
-                .then(Mono.defer { Mono.just(RestResponseVO(true)) })
+            // 1
+            .flatMap { boardDAO.isBbsUid(boardReqInsertDTO.bbsUid) }
+            .filter { exists -> exists }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
+            // 2
+            .map {
+                // 2
+                BoardInsertDTO(
+                    boardReqInsertDTO.bbsUid,
+                    boardReqInsertDTO.title,
+                    boardReqInsertDTO.contents,
+                    rcUser.uid
+                )
+            }
+            // 3
+            .flatMap { boardDAO.insert(it) }
+            // 4
+            .then(Mono.defer { Mono.just(RestResponseVO(true)) })
     }
 
     override fun update(rcUser: RcUserJwtClaims, boardReqUpdateDTO: BoardReqUpdateDTO): Mono<RestResponseVO<Void>> {
@@ -101,31 +120,31 @@ class BoardServiceImpl(
          * 4. 수정 DB
          */
         return boardDAO.isBbsPath(boardReqUpdateDTO.bbsPath)
-                // 1
-                .filter { exists -> exists }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
-                // 2
-                .filterWhen {
-                    boardDAO.isBoardUid(boardReqUpdateDTO.uid)
-                }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BOARD_EXCEPTION)))
-                // 3
-                .filterWhen {
-                    boardDAO.isWriterBoard(boardReqUpdateDTO.uid, rcUser.uid)
-                }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_MATCH_WRITER_UID_EXCEPTION)))
-                .map {
-                    BoardUpdateDTO(
-                            boardReqUpdateDTO.uid,
-                            boardReqUpdateDTO.title,
-                            boardReqUpdateDTO.contents
-                    )
-                }
-                // 4
-                .flatMap {
-                    boardDAO.update(it)
-                }
-                .then(Mono.defer { Mono.just(RestResponseVO(true)) })
+            // 1
+            .filter { exists -> exists }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
+            // 2
+            .filterWhen {
+                boardDAO.isBoardUid(boardReqUpdateDTO.uid)
+            }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BOARD_EXCEPTION)))
+            // 3
+            .filterWhen {
+                boardDAO.isWriterBoard(boardReqUpdateDTO.uid, rcUser.uid)
+            }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_MATCH_WRITER_UID_EXCEPTION)))
+            .map {
+                BoardUpdateDTO(
+                    boardReqUpdateDTO.uid,
+                    boardReqUpdateDTO.title,
+                    boardReqUpdateDTO.contents
+                )
+            }
+            // 4
+            .flatMap {
+                boardDAO.update(it)
+            }
+            .then(Mono.defer { Mono.just(RestResponseVO(true)) })
     }
 
     override fun delete(rcUser: RcUserJwtClaims, boardReqDeleteDTO: BoardReqDeleteDTO): Mono<RestResponseVO<Void>> {
@@ -136,24 +155,24 @@ class BoardServiceImpl(
          * 4. 삭제 DB
          */
         return boardDAO.isBbsPath(boardReqDeleteDTO.bbsPath)
-                // 1
-                .filter { exists -> exists }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
-                // 2
-                .filterWhen {
-                    boardDAO.isBoardUid(boardReqDeleteDTO.uid)
-                }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BOARD_EXCEPTION)))
-                // 3
-                .filterWhen {
-                    boardDAO.isWriterBoard(boardReqDeleteDTO.uid, rcUser.uid)
-                }
-                .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_MATCH_WRITER_UID_EXCEPTION)))
-                // 4
-                .flatMap {
-                    boardDAO.delete(boardReqDeleteDTO.uid)
-                }
-                .then(Mono.defer { Mono.just(RestResponseVO(true)) })
+            // 1
+            .filter { exists -> exists }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BBS_BOARD_EXCEPTION)))
+            // 2
+            .filterWhen {
+                boardDAO.isBoardUid(boardReqDeleteDTO.uid)
+            }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_FOUND_BOARD_EXCEPTION)))
+            // 3
+            .filterWhen {
+                boardDAO.isWriterBoard(boardReqDeleteDTO.uid, rcUser.uid)
+            }
+            .switchIfEmpty(Mono.error(RcException(RcErrorMessage.NOT_MATCH_WRITER_UID_EXCEPTION)))
+            // 4
+            .flatMap {
+                boardDAO.delete(boardReqDeleteDTO.uid)
+            }
+            .then(Mono.defer { Mono.just(RestResponseVO(true)) })
     }
 
 }
