eb4235d98b7f145ebf7cc76bf092d65f25dac219
cjw <cjw.git@gmail.com>
2024-06-03 00:03:50 +0900

    fix: FileName UUID 적용

--- src/main/kotlin/com/devcjw/reactivecommunity/file/service/impl/FileServiceImpl.kt
+++ src/main/kotlin/com/devcjw/reactivecommunity/file/service/impl/FileServiceImpl.kt
@@ -7,6 +7,7 @@ import com.devcjw.reactivecommunity.file.model.entity.FileEntity
 import com.devcjw.reactivecommunity.file.model.entity.FileInsertDTO
 import com.devcjw.reactivecommunity.file.repository.FileRepository
 import com.devcjw.reactivecommunity.file.service.FileService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
@@ -18,9 +19,10 @@ import java.util.*
 
 @Service
 class FileServiceImpl(
-    private val fileRepository: FileRepository,
-    private val fileDAO: FileDAO,
+        private val fileRepository: FileRepository,
+        private val fileDAO: FileDAO,
 ) : FileService {
+    private val logger = KotlinLogging.logger {}
 
     override fun upload(fileParts: Flux<FilePart>): Flux<RestResponseVO<FileRepListVO>> {
         /**
@@ -30,45 +32,52 @@ class FileServiceImpl(
          * 4. file md5 반환
          */
         return fileParts
-            .flatMap { filePart ->
-                val md5 = MessageDigest.getInstance("MD5")
-                filePart.content()
-                    .flatMap { dataBuffer ->
-                        val bytes = ByteArray(dataBuffer.readableByteCount())
-                        dataBuffer.read(bytes)
-                        Mono.just(bytes)
-                    }.reduce { acc, bytes ->
-                        md5.update(bytes)
-                        acc + bytes
-                    }
-                    .flatMap { allBytes ->
-                        val md5Hex = md5.digest().joinToString("") { "%02x".format(it) }
-                        val fileSize = allBytes.size
-                        fileRepository.findByMd5(md5Hex)
-                            .switchIfEmpty(
-                                Mono.defer {
-                                    val fileEntity = FileEntity(
-                                        uid = UUID.randomUUID().toString(),
-                                        path = "/tmp",
-                                        size = fileSize,
-                                        name = filePart.filename(),
-                                        md5 = md5Hex,
-                                        createdAt = now(),
-                                        updatedAt = now()
-                                    )
-                                    filePart.transferTo(File("/tmp/${filePart.filename()}")).thenReturn(fileEntity)
-                                }
-                            )
-                    }
-                    .flatMap {
-                        fileDAO.insert(FileInsertDTO(it.uid, it.path, it.name, it.size, it.md5))
-                            .then(Mono.just(it))
-                    }
-            }
-            .map {
-                FileRepListVO(it.md5)
-            }
-            .map { RestResponseVO(result = true, data = it) }
+                .flatMap { filePart ->
+                    val md5 = MessageDigest.getInstance("MD5")
+                    filePart.content()
+                            .flatMap { dataBuffer ->
+                                val bytes = ByteArray(dataBuffer.readableByteCount())
+                                dataBuffer.read(bytes)
+                                Mono.just(bytes)
+                            }.reduce { acc, bytes ->
+                                md5.update(bytes)
+                                acc + bytes
+                            }
+                            .flatMap { allBytes ->
+                                val md5Hex = md5.digest().joinToString("") { "%02x".format(it) }
+                                val fileSize = allBytes.size
+                                logger.info { "md5 : $md5Hex, size : $fileSize" }
+                                val uid = UUID.randomUUID().toString()
+                                val filename = UUID.randomUUID().toString()
+                                fileRepository.findByMd5(md5Hex)
+                                        .switchIfEmpty(Mono
+                                                .defer {
+                                                    logger.info { "no find db" }
+                                                    val fileEntity = FileEntity(
+                                                            uid = uid,
+                                                            path = "/tmp",
+                                                            size = fileSize,
+                                                            name = filename,
+                                                            md5 = md5Hex,
+                                                            createdAt = now(),
+                                                            updatedAt = now()
+                                                    )
+                                                    filePart.transferTo(File("/tmp/$filename"))
+                                                            .thenReturn(fileEntity)
+                                                }
+                                                .flatMap {
+                                                    logger.info { "insert file $it" }
+                                                    fileDAO.insert(FileInsertDTO(it.uid, it.path, it.name, it.size, it.md5))
+                                                            .then(Mono.just(it))
+                                                }
+                                        )
+                            }
+
+                }
+                .map {
+                    FileRepListVO(it.md5)
+                }
+                .map { RestResponseVO(result = true, data = it) }
 
         /*return fileParts.flatMap { filePart ->
             val md5 = MessageDigest.getInstance("MD5")
