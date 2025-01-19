497bea30ae1387984c961ff38f140d747c649bcd
cjw <cjw.git@gmail.com>
2025-01-19 18:44:20 +0900

    fix: 공통 환경코드 추가 및 수정 시 정규식 검증 - env id 는 영소문자 및 '.' 만 가능

--- src/main/java/com/cjw/reactivecommunityproject/web/system/environment_management/model/request/SystemEnvironmentManagementCreateVO.java
+++ src/main/java/com/cjw/reactivecommunityproject/web/system/environment_management/model/request/SystemEnvironmentManagementCreateVO.java
@@ -4,10 +4,20 @@ import com.cjw.reactivecommunityproject.common.spring.model.entity.RcCommonEnvCo
 import com.cjw.reactivecommunityproject.web.system.environment_management.validation.SystemEnvironmentManagementValidationGroup;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SystemEnvironmentManagementCreateVO(
         @NotBlank(groups = {SystemEnvironmentManagementValidationGroup.Create.class}, message = "환경코드 id는 공백일 수 없습니다.")
+        @Size(groups = {SystemEnvironmentManagementValidationGroup.Create.class},
+                max = 200,
+                message = "환경코드 id 는 200자 이하여야 합니다."
+        )
+        @Pattern(
+                regexp = "^[a-z.]+$",
+                groups = {SystemEnvironmentManagementValidationGroup.Create.class},
+                message = "환경코드 id는 영소문자 및 '.'만 포함할 수 있습니다."
+        )
         String id,
 
         @NotNull(groups = {SystemEnvironmentManagementValidationGroup.Create.class}, message = "환경코드 타입은 null 일 수 없습니다.")
--- src/main/java/com/cjw/reactivecommunityproject/web/system/environment_management/model/request/SystemEnvironmentManagementModifyVO.java
+++ src/main/java/com/cjw/reactivecommunityproject/web/system/environment_management/model/request/SystemEnvironmentManagementModifyVO.java
@@ -5,10 +5,20 @@ import com.cjw.reactivecommunityproject.common.spring.model.entity.RcCommonEnvCo
 import com.cjw.reactivecommunityproject.web.system.environment_management.validation.SystemEnvironmentManagementValidationGroup;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SystemEnvironmentManagementModifyVO(
         @NotBlank(groups = {SystemEnvironmentManagementValidationGroup.Modify.class}, message = "환경코드 id는 공백일 수 없습니다.")
+        @Size(groups = {SystemEnvironmentManagementValidationGroup.Modify.class},
+                max = 200,
+                message = "환경코드 id 는 200자 이하여야 합니다."
+        )
+        @Pattern(
+                regexp = "^[a-z.]+$",
+                groups = {SystemEnvironmentManagementValidationGroup.Modify.class},
+                message = "환경코드 id는 영소문자 및 '.'만 포함할 수 있습니다."
+        )
         String id,
 
         @NotNull(groups = {SystemEnvironmentManagementValidationGroup.Modify.class}, message = "환경코드 타입은 null 일 수 없습니다.")
