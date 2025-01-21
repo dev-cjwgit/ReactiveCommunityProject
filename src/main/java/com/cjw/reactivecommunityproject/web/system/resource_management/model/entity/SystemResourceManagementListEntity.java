99c9fd857c3eb2e65bd96e7063988042331949e9
cjw <cjw.git@gmail.com>
2025-01-21 23:05:58 +0900

    fix: 리소스 조회 모델 정보 추가 - enabled 추가

--- src/main/java/com/cjw/reactivecommunityproject/web/system/resource_management/model/entity/SystemResourceManagementListEntity.java
+++ src/main/java/com/cjw/reactivecommunityproject/web/system/resource_management/model/entity/SystemResourceManagementListEntity.java
@@ -1,5 +1,6 @@
 package com.cjw.reactivecommunityproject.web.system.resource_management.model.entity;
 
+import com.cjw.reactivecommunityproject.common.spring.model.entity.CommonEnabledEnum;
 import com.cjw.reactivecommunityproject.common.spring.model.entity.RcManageResourceMethodEnum;
 
 import java.time.ZonedDateTime;
@@ -8,6 +9,7 @@ public record SystemResourceManagementListEntity(
         Long uid,
         RcManageResourceMethodEnum method,
         String urlPattern,
+        CommonEnabledEnum enabled,
         String createdUserUid,
         ZonedDateTime createdUtcAt,
         String updatedUserUid,
