1c933e512df50d20ed6febca7200e42bca230711
CJW <cjw.git@gmail.com>
2025-08-03 16:13:03 +0900

    fix: log4j2 mybatis logging 복구

--- src/main/java/com/cjw/reactivecommunityproject/common/spring/config/db/MariaDBConfig.java
+++ src/main/java/com/cjw/reactivecommunityproject/common/spring/config/db/MariaDBConfig.java
@@ -1,16 +1,8 @@
 package com.cjw.reactivecommunityproject.common.spring.config.db;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -21,34 +13,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 @RequiredArgsConstructor
 public class MariaDBConfig {
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
-    public HikariConfig hikariConfig() {
-        return new HikariConfig();
-    }
-
-    @Bean(name = "dataSource")
-    public HikariDataSource hikariDataSource(@Autowired HikariConfig hikariConfig) {
-        return new HikariDataSource(hikariConfig);
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(
-            @Autowired DataSource dataSource
-            , ApplicationContext applicationContext
-    ) throws Exception {
-        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setTypeAliasesPackage("com.cjw.reactivecommunityproject");
-        factoryBean.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml"));
-        factoryBean.setMapperLocations(applicationContext.getResources("classpath:mapper/**/*.xml"));
-        return factoryBean.getObject();
-    }
-
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate(@Autowired @Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
 
     @Bean("txManager")
     public PlatformTransactionManager transactionManager(@Autowired DataSource dataSource) {
--- src/main/java/com/cjw/reactivecommunityproject/server/kafka/orders/model/KafkaOrdersVO.java
+++
@@ -1,8 +0,0 @@
-package com.cjw.reactivecommunityproject.server.kafka.orders.model;
-
-public record KafkaOrdersVO(
-        Long id,
-        String item,
-        Integer count
-) {
-}
