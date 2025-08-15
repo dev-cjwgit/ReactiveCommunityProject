3a9dc38420202629cf2d95e1919545eaaeff8221
CJW <cjw.git@gmail.com>
2025-08-15 10:06:21 +0900

    refactor: rest template 삭제 - web client 및 rest client 도입

---
+++ src/main/java/com/cjw/reactivecommunityproject/common/spring/config/client/ClientConfig.java
@@ -0,0 +1,95 @@
+package com.cjw.reactivecommunityproject.common.spring.config.client;
+
+
+import com.cjw.reactivecommunityproject.common.spring.config.properties.RcProperties;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import java.util.concurrent.TimeUnit;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestClientCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+@Configuration
+@EnableConfigurationProperties(RcProperties.class)
+public class ClientConfig {
+
+    @Bean("webClient")
+    public WebClientCustomizer webClientCustomizer(RcProperties rcProperties) {
+        return builder -> {
+            var provider = ConnectionProvider.builder("global-http")
+                    .maxConnections(rcProperties.http().pool().maxConnections())
+                    .maxIdleTime(rcProperties.http().pool().maxIdleTime())
+                    .pendingAcquireMaxCount(rcProperties.http().pool().pendingAcquireMaxCount())
+                    .build();
+
+            HttpClient httpClient = HttpClient.create(provider)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) rcProperties.http().timeouts().connect().toMillis())
+                    .responseTimeout(rcProperties.http().timeouts().response())
+                    .doOnConnected(conn -> conn
+                            .addHandlerLast(new ReadTimeoutHandler(rcProperties.http().timeouts().read().toMillis(), TimeUnit.MILLISECONDS))
+                            .addHandlerLast(new WriteTimeoutHandler(rcProperties.http().timeouts().write().toMillis(), TimeUnit.MILLISECONDS))
+                    );
+
+            builder.clientConnector(new ReactorClientHttpConnector(httpClient))
+                    .codecs(c -> c.defaultCodecs().maxInMemorySize((int) rcProperties.http().codecs().maxInMemorySize().toBytes()))
+                    .filter(logRequest())
+                    .filter(logResponse());
+        };
+    }
+
+    @Bean("restClient")
+    public RestClientCustomizer restClientCustomizer(RcProperties rc) {
+        return builder -> {
+            var connCfg = ConnectionConfig.custom()
+                    .setConnectTimeout(Timeout.ofMilliseconds(rc.http().timeouts().connect().toMillis()))
+                    .setSocketTimeout(Timeout.ofMilliseconds(rc.http().timeouts().read().toMillis()))
+                    .build();
+
+            var cm = PoolingHttpClientConnectionManagerBuilder.create()
+                    .setDefaultConnectionConfig(connCfg)
+                    .setMaxConnTotal(rc.http().pool().maxConnections())
+                    .setMaxConnPerRoute(rc.http().pool().maxConnections())
+                    .build();
+
+            var reqCfg = RequestConfig.custom()
+                    .setResponseTimeout(Timeout.ofMilliseconds(rc.http().timeouts().response().toMillis()))
+                    .setConnectionRequestTimeout(Timeout.ofMilliseconds(rc.http().timeouts().request().toMillis()))
+                    .build();
+
+            CloseableHttpClient apache = HttpClients.custom()
+                    .setConnectionManager(cm)
+                    .setDefaultRequestConfig(reqCfg)
+                    .evictIdleConnections(TimeValue.ofMilliseconds(rc.http().pool().maxIdleTime().toMillis()))
+                    .evictExpiredConnections()
+                    .build();
+
+            ClientHttpRequestFactory rf = new HttpComponentsClientHttpRequestFactory(apache);
+            builder.requestFactory(rf);
+        };
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return (req, next) -> next.exchange(req);
+    }
+
+    private ExchangeFilterFunction logResponse() {
+        return (req, next) -> next.exchange(req)
+                .doOnNext(res -> { /* 상태코드/지연시간 로깅 등 */ });
+    }
+}
--- src/main/java/com/cjw/reactivecommunityproject/common/spring/config/properties/RcProperties.java
+++ src/main/java/com/cjw/reactivecommunityproject/common/spring/config/properties/RcProperties.java
@@ -1,21 +1,57 @@
 package com.cjw.reactivecommunityproject.common.spring.config.properties;
 
+import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.unit.DataSize;
 
 
 @ConfigurationProperties(prefix = "rc")
 public record RcProperties(
-        ConfigProperties config
-        , CacheManageProperties cacheManage
+        ConfigProperties config,
+        CacheManageProperties cacheManage,
+        SchedulerProperties scheduler,
+        HttpProperties http
 ) {
     public record ConfigProperties(
+            String defaultLanguage,
             String defaultRegion
-            , String defaultLanguage
-    ) {
-    }
+    ) {}
 
     public record CacheManageProperties(
             int expiresMinutes
+    ) {}
+
+    public record SchedulerProperties(
+            BatchProperties batchCacheReload,
+            BatchProperties batchCacheOk
     ) {
+        public record BatchProperties(
+                String cron,
+                boolean enabled
+        ) {}
+    }
+
+    public record HttpProperties(
+            Timeouts timeouts,
+            Pool pool,
+            Codecs codecs
+    ) {
+        public record Timeouts(
+                Duration connect,
+                Duration response,
+                Duration read,
+                Duration write,
+                Duration request
+        ) {}
+
+        public record Pool(
+                int maxConnections,
+                Duration maxIdleTime,
+                int pendingAcquireMaxCount
+        ) {}
+
+        public record Codecs(
+                DataSize maxInMemorySize
+        ) {}
     }
 }
