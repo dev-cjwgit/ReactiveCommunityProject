d4584bdd7e947b693d73eca45e3f49025d874b27
CJW <cjw.git@gmail.com>
2025-08-16 22:11:14 +0900

    feat: 패키지 추가 기본 구조 반영

---
+++ src/main/java/com/cjw/reactivecommunityproject/web/board/validation/BoardValidationGroup.java
@@ -0,0 +1,14 @@
+package com.cjw.reactivecommunityproject.web.board.validation;
+
+import jakarta.validation.groups.Default;
+
+public class BoardValidationGroup {
+    private BoardValidationGroup() {
+    }
+
+    public interface Create extends Default {
+    }
+
+    public interface Modify extends Default {
+    }
+}
