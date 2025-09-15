a0514d6f44d16fb222af6086ab564992cd7fbb22
CJW <cjw.git@gmail.com>
2025-09-15 18:27:16 +0900

    fix: Board 기본 구조 수정

--- src/main/java/com/cjw/reactivecommunityproject/web/board/controller/BoardController.java
+++ src/main/java/com/cjw/reactivecommunityproject/web/board/controller/BoardController.java
@@ -10,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/board/{bbs}")
 public class BoardController {
     private final BoardService boardService;
+
+
 }
